f82af1af88ab7b8789e1364bd23303eff9ff0e6c
Yash Malshe <yashamalshe@gmail.com>
2025-06-01 00:36:26 -0400

    Added Full git integration as shown in https://www.youtube.com/watch?v=zOQMwWqdp9w

--- lua/plugins/gitconfig.lua
+++ lua/plugins/gitconfig.lua
@@ -4,9 +4,10 @@ return{
     config = function ()
       require("gitsigns").setup()
       vim.keymap.set('n', "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set('n', "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
     end
   },
   {
-
+    "tpope/vim-fugitive",
   }
 }
